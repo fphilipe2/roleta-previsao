4a5879492ce9c65fe347e42b4a7a6384c8a2d394
fphilipe2 <fernandophilipe2@gmail.com>
2025-06-17 12:51:05 -0300

    app.py

--- app.py
+++ app.py
@@ -35,43 +35,8 @@ def obter_alto_baixo(numero):
     return 'baixo' if numero <= 18 else 'alto'
 
 # Lista de números proibidos (mesmo conteúdo)
-numeros_proibidos = {
-    1: [3, 8, 11, 12, 13, 28, 29, 30, 35, 36],
-    36: [3, 8, 11, 12, 13, 28, 29, 30, 35, 36],
-    2: [3, 7, 8, 11, 12, 23, 26, 28, 30, 35, 36],
-    20: [3, 7, 8, 11, 12, 23, 26, 28, 30, 35, 36],
-    3: [1, 2, 9, 14, 17, 20, 21, 22, 25, 31, 34],
-    8: [1, 2, 9, 14, 17, 20, 21, 22, 25, 31, 34],
-    4: [7, 11, 12, 13, 18, 27, 28, 29, 30, 35],
-    33: [7, 11, 12, 13, 18, 27, 28, 29, 30, 35],
-    5: [6, 7, 9, 13, 18, 27, 28, 29, 30, 35, 36],
-    32: [6, 7, 9, 13, 18, 27, 28, 29, 30, 35, 36],
-    6: [0, 3, 10, 15, 16, 19, 23, 24, 26, 32],
-    18: [0, 3, 10, 15, 16, 19, 23, 24, 26, 32],
-    7: [1, 2, 4, 15, 16, 19, 20, 21, 24, 32, 33],
-    9: [0, 3, 5, 8, 10, 23, 24, 26, 30, 32, 35],
-    17: [0, 3, 5, 8, 10, 23, 24, 26, 30, 32, 35],
-    10: [6, 9, 14, 17, 18, 22, 25, 27, 29, 31, 34],
-    0: [6, 9, 14, 17, 18, 22, 25, 27, 29, 31, 34],
-    12: [1, 2, 4, 14, 16, 19, 20, 21, 25, 31, 33],
-    13: [1, 4, 5, 15, 16, 19, 20, 21, 24, 32, 33],
-    14: [0, 3, 8, 10, 11, 12, 23, 26, 28, 30, 35],
-    25: [0, 3, 8, 10, 11, 12, 23, 26, 28, 30, 35],
-    15: [6, 7, 9, 13, 18, 22, 27, 28, 29, 34, 36],
-    24: [6, 7, 9, 13, 18, 22, 27, 28, 29, 34, 36],
-    16: [6, 7, 11, 12, 13, 18, 22, 27, 28, 29, 36],
-    19: [6, 7, 11, 12, 13, 18, 22, 27, 28, 29, 36],
-    22: [0, 3, 5, 8, 10, 15, 16, 23, 24, 26, 32],
-    23: [2, 6, 9, 14, 17, 18, 20, 22, 25, 31, 34],
-    26: [2, 6, 9, 14, 17, 18, 20, 22, 25, 31, 34],
-    27: [0, 1, 4, 5, 10, 15, 16, 19, 24, 32, 33],
-    29: [0, 1, 4, 5, 10, 15, 16, 19, 24, 32, 33],
-    28: [1, 2, 4, 14, 15, 16, 19, 20, 21, 24, 33],
-    30: [1, 2, 4, 9, 14, 15, 17, 21, 25, 31, 33],
-    35: [1, 2, 4, 9, 14, 15, 17, 21, 25, 31, 33],
-    31: [0, 3, 5, 8, 10, 11, 12, 23, 26, 30, 35],
-    34: [0, 3, 5, 8, 10, 23, 24, 26, 30, 32, 35],
-}
+from numeros_proibidos import numeros_proibidos  # Supondo que você mova esse dicionário para outro arquivo
+
 # Armazenamento do histórico completo
 if 'historico' not in st.session_state:
     st.session_state.historico = []
@@ -97,17 +62,36 @@ if st.button("Exportar histórico CSV"):
 # Estratégia Reflexiva
 resultado_reflexivo = []
 palpites = []
+reflexo_por_numero = {i: [] for i in range(37)}
+
 for i in range(1, len(st.session_state.historico)):
     ant = st.session_state.historico[i-1]
     atual = st.session_state.historico[i]
     if ant in numeros_proibidos and atual in numeros_proibidos[ant]:
         resultado_reflexivo.append("X")
+        reflexo_por_numero[atual].append("X")
     else:
         resultado_reflexivo.append("1")
+        reflexo_por_numero[atual].append("1")
         palpites.append((i, ant, [n for n in range(37) if n not in numeros_proibidos.get(ant, [])]))
 
-st.subheader("Reflexiva (1 ou X)")
-st.markdown(", ".join(resultado_reflexivo))
+st.subheader("Reflexiva por número (últimos 5 resultados)")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.markdown("**0 a 11**")
+    for n in range(0, 12):
+        st.write(f"{n} = {' '.join(reflexo_por_numero[n][-5:])}")
+
+with col2:
+    st.markdown("**12 a 24**")
+    for n in range(12, 25):
+        st.write(f"{n} = {' '.join(reflexo_por_numero[n][-5:])}")
+
+with col3:
+    st.markdown("**25 a 36**")
+    for n in range(25, 37):
+        st.write(f"{n} = {' '.join(reflexo_por_numero[n][-5:])}")
 
 # Exibir os palpites mais recentes (até 5)
 if palpites:
@@ -143,8 +127,9 @@ def alertar_repeticoes(tipo):
             contagem = 1
             ultimo = atual
 
-alertar_repeticoes(obter_duzia)
-alertar_repeticoes(obter_coluna)
-alertar_repeticoes(obter_cor)
-alertar_repeticoes(obter_paridade)
-alertar_repeticoes(obter_alto_baixo)
+if st.session_state.historico:
+    alertar_repeticoes(obter_duzia)
+    alertar_repeticoes(obter_coluna)
+    alertar_repeticoes(obter_cor)
+    alertar_repeticoes(obter_paridade)
+    alertar_repeticoes(obter_alto_baixo)
