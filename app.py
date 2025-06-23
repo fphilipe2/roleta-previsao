b5c9df65348636b30370bc17bf68d7c47dff3e07
fphilipe2 <fernandophilipe2@gmail.com>
2025-06-23 20:05:10 -0300

    app.py

--- app.py
+++ app.py
@@ -40,6 +40,17 @@ numeros_proibidos = {
     31: [0, 3, 5, 8, 10, 11, 12, 23, 26, 30, 35],
     34: [0, 3, 5, 8, 10, 23, 24, 26, 30, 32, 35],
 }
+duzias = {
+    'D1': list(range(1, 13)),
+    'D2': list(range(13, 25)),
+    'D3': list(range(25, 37))
+}
+
+colunas = {
+    'C1': [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
+    'C2': [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
+    'C3': [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+}
 # Funções auxiliares para as NOVAS estratégias
 def identificar_dúzia(num):
     if num == 0: return '0'
