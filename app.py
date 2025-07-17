680e8389de7950f3fb427a5bbc433fcf562f2a20
fphilipe2 <fernandophilipe2@gmail.com>
2025-07-16 21:45:21 -0300

    app.py

--- app.py
+++ app.py
@@ -75,10 +75,12 @@ else:
     arquivo = st.file_uploader("Envie um arquivo CSV com uma coluna chamada 'número'", type=["csv"])
     if arquivo:
         try:
-            try:
-    df = pd.read_csv(arquivo_carregado)
-except UnicodeDecodeError:
-    df = pd.read_csv(arquivo_carregado, encoding='ISO-8859-1')
+            with st.expander("Carregar arquivo"):
+    try:
+        df = pd.read_csv(arquivo_carregado)
+    except UnicodeDecodeError:
+        df = pd.read_csv(arquivo_carregado, encoding='ISO-8859-1')
+
 
             if "número" not in df.columns:
                 st.error("O CSV deve conter uma coluna chamada 'número'")
